5d50b13f645c096ece653cc4907b763688f7d378
AndresMGdev <cmendoza022002@gmail.com>
2024-06-05 19:32:41 -0500

    created custom hook useInput for forms

---
+++ src/hooks/useInput.tsx
@@ -0,0 +1,27 @@
+import { useState, ChangeEvent } from 'react';
+
+interface UseFieldResult {
+  type: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  reset: () => void;
+}
+
+export const useInput = (type: string): UseFieldResult => {
+  const [value, setValue] = useState<string>('');
+  
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+  
+  const reset = () => {
+    setValue('');
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+    reset
+  };
+};
