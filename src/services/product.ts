67dda0685d8886a121d5e4c2284dd92de9e69346
AndresMGdev <cmendoza022002@gmail.com>
2024-06-06 18:38:04 -0500

    changes

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,6 +1,6 @@
 import { Product } from '@/types/product';
 import { User } from '@/types/user';
-import { mockProducts } from './productjson';
+import { mockProducts } from '../utils/products';
 
 // const API_URL = 'https://api.ficticia.com/products';
 
@@ -16,20 +16,6 @@ export const fetchProductsData = async (user: User): Promise<Product[] | null> =
         return null;
     }
 };
-
-// const mockProduct: Product = {
-//     id: '1',
-//     name: 'Macbook Air 13 M1',
-//     productType: 'Laptop Mac',
-//     quantity: 10,
-//     price: 1799.99,
-//     SKU: 'ELEC-001',
-//     supplier: 'Apple',
-//     geoCoordinates: {
-//         latitude: 37.7749,
-//         longitude: -122.4194,
-//     },
-// };
 
 export const fetchProductData = async (productId: string): Promise<Product | null> => {
     // Simulating API call delay
