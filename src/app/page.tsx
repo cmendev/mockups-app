dbc7fbd740390e73a777eae4f981913972ab1282
AndresMGdev <cmendoza022002@gmail.com>
2024-06-13 18:19:34 -0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="container md:pt-48 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
 
           <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
-            <h1 className="text-3xl md:text-5xl text-indigo-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Welcome to Pro<span className="bg-accent">Stock</span></h1>
+            <h1 className="text-3xl md:text-5xl text-indigo-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Welcome to ProStock</h1>
             <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">The place where you can manage your product inventory dynamically!</p>
           </div>
         </div>
